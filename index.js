34db820a79954f9416d45bb43aad37b47a471f57
kara <kdiaby.pro@gmail.com>
2019-05-09 15:54:38 +0200

    cron setup to launch in prod

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const createHubspotProspects = async url => {
         .catch(err => console.log(err));
 };
 
-// setup the cron job every 2,3,4 days of the week on 19h (utc)
-cron.schedule("53 13 * * Tuesday-Thursday", () => {
+// setup the cron job every 2,3,4 days of the week on 18h (utc)
+cron.schedule("00 18 * * Tuesday-Thursday", () => {
     createHubspotProspects(process.env.HIVE_AUTOPROSP_URL);
 });
